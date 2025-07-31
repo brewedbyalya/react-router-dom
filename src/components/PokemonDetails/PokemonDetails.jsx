2ec5cc5d8c07dc8956b624bf358572338bf80823
alya <aliya2002mah@gmail.com>
2025-07-31 11:28:22 +0300

    done, some small issue

---
+++ src/components/PokemonDetails/PokemonDetails.jsx
@@ -0,0 +1,27 @@
+const PokemonDetails = (props) => {
+  console.log(props); 
+
+ const { pokemonId } = useParams();
+  console.log('pokemonId:', pokemonId);
+
+
+  const singlePokemon = props.pokemon.find((poke) => (
+    poke._id === Number(pokemonId)
+  ));
+  console.log('Pokemon Object:', singlePokemon);
+
+  return (
+    <>
+      <h2>{singlePokemon.name}</h2>
+      <dl>
+        <dt>Weight:</dt>
+        <dd>{singlePokemon.weight}</dd>
+        <dt>Height:</dt>
+        <dd>{singlePokemon.height}</dd>
+      </dl>
+    </>
+  );
+
+};
+
+export default PokemonDetails;
